4d37e8ff82e2bfa28c74bdf85d8ab4b4d940048b
Helmut Minst <helmut.minst@valtech.com>
2024-04-09 12:55:47 +0200

    little css

--- blocks/dynamic-menu/dynamic-menu.js
+++ blocks/dynamic-menu/dynamic-menu.js
@@ -1,4 +1,10 @@
 export default function decorate(block) {
- console.log(block)
+  console.log(block);
+  const nutritions = block.querySelectorAll('[data-property="nutrition"]');
+  nutritions?.forEach((nutrition) => {
+    let headline = document.createElement("div");
+    headline.innerHTML = "nutitional data:";
+    headline.setAttribute("class", "nutitional_headline");
+    nutrition.prepend(headline);
+  });
 }
-
